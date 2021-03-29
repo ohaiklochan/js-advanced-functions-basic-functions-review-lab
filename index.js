6a72308aa2a300b6828c5a99d88803dd034374fc
ohaiklochan <caforonda103@yahoo.com>
2021-03-28 22:45:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+};
+
+const mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(flair="*") {
+    return function (msg="special") {
+        return `You are ${flair}${msg}${flair}!`;
+    }
+};
+
+const Calculator = {
+    add : function(x, y) {
+        return x + y;
+    },
+    subtract : function(x, y) {
+        return x - y;
+    },
+    multiply: function(x, y) {
+        return x * y;
+    },
+    divide : function(x, y) {
+        return x / y;
+    }
+};
+
+function actionApplyer(integer, actions){
+    actions.forEach(func => {
+        integer = func(integer)
+    });
+    return integer
+};
